fa086e35381418596750ff9f960db7d11850dd23
ejuriss <ehjuriss@gmail.com>
2023-02-22 16:18:22 +1300

    Final Website
    Wonka Website

---
+++ wonkawebsite/main.js
@@ -0,0 +1,15 @@
+// jQuery Smooth Scroll
+$('.nav a').on('click', function(e) {
+    if(this.hash !== '') {
+        e.preventDefault();
+        
+        var hash = this.hash;
+        
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            }, 
+            800
+        );
+    }
+});
